9a595786a5494fca0f0b299546278d06fec03202
TMTimSmans <r0994452@student.thomasmore.be>
2025-03-26 12:58:12 +0200

    Added table creation

--- api/services/session_service.py
+++ api/services/session_service.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from models.base import Base
 from config import DATABASE_URL
 
 
 engine = create_engine(DATABASE_URL)
 
+Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
