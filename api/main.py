ceec5af26cfa74d533c081cd96443262bf550f86
TMTimSmans <r0994452@student.thomasmore.be>
2025-03-26 13:14:21 +0200

    Fixed a bug

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
 from fastapi import Depends, FastAPI
+from sqlalchemy import create_engine
+from config import DATABASE_URL
+from models.base import Base
 from routers import student_routes, attendance_routes
 
 app = FastAPI(
@@ -10,3 +13,8 @@ app = FastAPI(
 
 app.include_router(student_routes.router, prefix="/student", tags=["Students"])
 app.include_router(attendance_routes.router, prefix="/attendance", tags=["Attendance"])
+
+
+engine = create_engine(DATABASE_URL)
+
+Base.metadata.create_all(engine)
