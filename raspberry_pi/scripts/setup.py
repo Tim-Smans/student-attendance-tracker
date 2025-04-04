a04714413e8b787d6a748969592ec12f625f77db
TMTimSmans <r0994452@student.thomasmore.be>
2025-04-04 15:48:11 +0300

    updated the setup of a device

--- raspberry_pi/scripts/setup.py
+++ raspberry_pi/scripts/setup.py
@@ -1,1 +1,26 @@
+import sys
+from api.client import post
+
 BASE_URL = "https://student-attendance-tracker-ungw.onrender.com/"
+
+if len(sys.argv) < 3:
+    print("Usage: python setup.py <room_name> <device_identifier> <api_key>")
+    exit(1)
+
+room_name = sys.argv[1]
+device_identifier = sys.argv[2]
+API_KEY = sys.argv[3]
+
+
+headers = {
+    "x-api-key": f"{API_KEY}"
+}
+
+res = post("room_device", {"room_name": room_name, "device_identifier": device_identifier}, headers=headers)
+
+if res.status_code != 201:
+  print(res.text)
+  exit(1)
+
+
+print("Room device created successfully!")
--- raspberry_pi/setup.sh
+++ raspberry_pi/setup.sh
@@ -1,3 +1,29 @@
+set -e
+
+
+if ! command -v yq &> /dev/null
+then
+    echo "🔍 yq not found, installing temporarily..."
+    wget -qO yq "https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64"
+    chmod +x yq
+fi
+
+ROOM_NAME=$(./yq  '.room_name' $CONFIG_FILE)
+DEVICE_IDENTIFIER=$(./yq '.device_identifier' $CONFIG_FILE)
+API_KEY=$(./yq '.api_key' $CONFIG_FILE)
+
+
+# 👉 Als device_identifier nog "EDIT ME!" is, pak dan automatisch serial
+if [ "$DEVICE_IDENTIFIER" = "\"\"" ]; then
+    echo "🎯 DEVICE_IDENTIFIER was not set, generating from CPU Serial..."
+    DEVICE_IDENTIFIER=$(awk '/Serial/ {print $3}' /proc/cpuinfo)
+fi
+
+sudo apt u
+
+#Reading Config
+CONFIG_FILE="config.yaml"
+
 sudo apt update
 sudo apt install libzbar0
 
@@ -5,5 +31,7 @@ python3 -m venv .venv --system-site-packages
 source .venv/bin/activate
 
 pip install -r requirements.txt
+
+python3 setup.py $ROOM_NAME $DEVICE_IDENTIFIER $API_KEY
 
 echo "Finished setting up student attendance tracker on this device!"
