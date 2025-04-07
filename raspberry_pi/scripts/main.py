138f439bd5bcda9fd342e963eae46e241d6d31c7
TMTimSmans <r0994452@student.thomasmore.be>
2025-04-07 15:50:18 +0300

    added another video script for testing purposes

--- raspberry_pi/scripts/main.py
+++ raspberry_pi/scripts/main.py
@@ -1,42 +1,72 @@
-from picamera2 import Picamera2
 import cv2
-from pyzbar.pyzbar import decode
-from utils.image_utils import is_blurry
-from utils.camera import init_camera
-from utils.decoder import decode_qr
+import pytesseract
+import re
+import time
+import os
+import numpy as np
 
-# Initiasing the camera, configuring size, format and alignment.
-picam2 = init_camera()
-
-print("Scanning QR codes... Press 'q' to quit.")
-
-previous_data = None
-last_scan_time = 0
-
-
-# Main loop, will keep running until it receives the 'q' key
 while True:
-    # Creates a snapshot of the camera
-    frame = picam2.capture_array()
-    
-    # Check scherpte
-    if is_blurry(frame):
-        cv2.putText(frame, "Too Blurry - move the QR code", (40, 50),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.imshow("QR Scanner", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        continue
-    
-    # QR decoding
-    qr_codes = decode(frame)
-    previous_data, last_scan_time = decode_qr(qr_codes, frame, previous_data, last_scan_time)    
-    
-    # Show frame to the user
-    cv2.imshow("QR Scanner", frame)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    key = input("Druk ENTER om een foto te nemen, of typ 'q' om te stoppen: ")
+
+    if key == 'q':
         break
 
-picam2.close()
-cv2.destroyAllWindows()
+    # When the user presses enter it will take a picture
+    timestamp = int(time.time())
+    filename = f"capture_{timestamp}.jpg"
+    os.system(f'libcamera-jpeg -o {filename} --width 1280 --height 720 --nopreview')
+
+    # Load the picture into OpenCV
+    image = cv2.imread(filename)
+    if image is None:
+        print("Error: No image found  .")
+        continue
+
+    # Preprocessing the image
+    # 1. put the picture into grayscale
+    # 2. Better contract with CLAHE (Contrast limited adaptive histogram equalization)
+    # 3. Gaussian blur to reduce noise
+    # 4. Thresholding (we want to focus on the text) 130 is the threshold everything above 130 becomes white, everything under 130 becomes black
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    enhanced = clahe.apply(gray)
+    blurred = cv2.GaussianBlur(enhanced, (3, 3), 0)
+    _, processed = cv2.threshold(blurred, 130, 255, cv2.THRESH_BINARY)
+
+    # Save the prepocessed image (just for debugging and tweaking)
+    processed_filename = f"preprocessed_{timestamp}.png"
+    cv2.imwrite(processed_filename, processed)
+    print(f"Preprocessed opgeslagen als: {processed_filename}")
+
+    # Using the OCR model to extract the text and put it into a variable
+    custom_config = r'--oem 3 --psm 6'
+    raw_text = pytesseract.image_to_string(processed, config=custom_config)
+
+    # Find all numbers in the image using regex
+    numbers = re.findall(r'\d{6,7}', raw_text)
+
+    # Initialise IDs
+    student_id = None
+    peppi_id = None
+
+    # Check if the number is 6 or 7 digits, if it is 6 it is a peppi id, if it is 7 it is a student id (Temporary workaround)
+    for num in numbers:
+        if len(num) == 7:
+            student_id = num
+        elif len(num) == 6:
+            peppi_id = num
+
+    # Output logs
+    print("\n📋 Scanned text:")
+    print(raw_text.strip())
+
+    print("\n Extracted IDs:")
+    if student_id:
+        print(f"Student ID: {student_id}")
+    else:
+        print("No Student ID found.")
+
+    if peppi_id:
+        print(f"Peppi ID: {peppi_id}")
+    else:
+        print("No Peppi ID found.")
--- raspberry_pi/scripts/utils/image_utils.py
+++
@@ -1,5 +0,0 @@
-import cv2
-
-def is_blurry(img, threshold=100.0):
-    laplacian_var = cv2.Laplacian(img, cv2.CV_64F).var()
-    return laplacian_var < threshold
