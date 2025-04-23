47f1b92899071c663ab3056d91d15d895c2078ae
TMTimSmans <r0994452@student.thomasmore.be>
2025-04-23 10:50:25 +0300

    Changed api route

--- raspberry_pi/scripts/ping_backend.py
+++ raspberry_pi/scripts/ping_backend.py
@@ -7,7 +7,7 @@ from utils.device_utils import get_device_identifier
 API_KEY = os.getenv("API_KEY")
 
 PI_ID = get_device_identifier()  # unieke naam of UUID
-API_URL = "https://student-attendance-tracker-ungw.onrender.com/api/ping"
+API_URL = "https://student-attendance-tracker-ungw.onrender.com/status/ping"
 
 try:
     requests.post(API_URL, json={"id": PI_ID}, headers={"x-api-key": API_KEY})
