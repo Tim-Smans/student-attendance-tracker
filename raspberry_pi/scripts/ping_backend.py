ab9e5ded531105b987aab7477bfab7c9c88ab1c3
TMTimSmans <r0994452@student.thomasmore.be>
2025-04-23 10:47:39 +0300

    Changed backend code

--- raspberry_pi/scripts/ping_backend.py
+++ raspberry_pi/scripts/ping_backend.py
@@ -1,13 +1,15 @@
 
 
+import os
 import requests
 
-from scripts.utils.device_utils import get_device_identifier
+from utils.device_utils import get_device_identifier
+API_KEY = os.getenv("API_KEY")
 
 PI_ID = get_device_identifier()  # unieke naam of UUID
 API_URL = "https://student-attendance-tracker-ungw.onrender.com/api/ping"
 
 try:
-    requests.post(API_URL, json={"id": PI_ID})
+    requests.post(API_URL, json={"id": PI_ID}, headers={"x-api-key": API_KEY})
 except Exception:
     pass
